8b4873e73b892c004aeed034019fee746a58f535
faizulable <enterprisefab@gmail.com>
2021-04-25 16:22:04 +0530

    HomePage work has started

--- lib/screens/home/myHomePage.dart
+++ lib/screens/home/myHomePage.dart
@@ -3,18 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:fabtech_aspirationclass_dev/wrapper.dart';
 import 'package:fabtech_aspirationclass_dev/main.dart';
 import 'package:fabtech_aspirationclass_dev/models/appPref.dart';
+import 'package:fabtech_aspirationclass_dev/customPainter/LabelCustomPainter.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the screens.home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -23,85 +15,132 @@ class MyHomePage extends StatefulWidget {
 }
 class _MyHomePageState extends State<MyHomePage> {
   AuthService _auth = AuthService();
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: [
-          ElevatedButton.icon(
-              icon: Icon(Icons.exit_to_app),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 5),
+            child: ElevatedButton.icon(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.teal.shade400),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20),),
+                  ),
+                ),
+              ),
+              icon: Icon(Icons.exit_to_app_rounded),
+              label: Text('Logout'),
               onPressed: () async {
                 await _auth.signOut();
+                //Goto the the login page
                 Navigator.pushAndRemoveUntil(
                   context,
                   MaterialPageRoute(builder: (context) => Wrapper()),
                       (Route<dynamic> route) => false,
                 );
               },
-              label: Text('logout'),
+            ),
           ),
         ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.deepPurple.shade600, Colors.teal.shade400]
+          ),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5,horizontal: 7),
+              height: 110.0,
+              decoration: BoxDecoration(
+                color: Colors.teal.shade100,
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+                shape: BoxShape.rectangle,
+              ),
+              child: SingleChildScrollView(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5,horizontal: 5.0),
+                      width: 100.0,
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          image: DecorationImage(
+                              image: AssetImage('images/emblame.png'),
+                              fit: BoxFit.fill
+                          ),
+                          shape: BoxShape.circle
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: Center(
+                                child: Text(sp.getString(AppPref.centerNamePref),
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontFamily: 'LemonMilkBold',
+                                  ),
+                                ),
+                              ),
+                              width: 280.0,
+                            ),
+                            Container(
+                              width: 250,
+                              child: Center(
+                                child: Text(sp.getString(AppPref.emailPref),
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontFamily: 'LandasansMedium',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 10),
+              child: CustomPaint(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Text('List of classes',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontFamily: 'Overtaking',
+                      ),
+                    ),
+                  ),
+                ),
+                painter: RPSCustomPainter(),
+              ),
             ),
-            Text(sp.getString(AppPref.centerNamePref) ?? 'BLANK'),
-            Text(sp.getString(AppPref.emailPref) ?? 'BLANK'),
-            Text(sp.getString(AppPref.centerIdPref) ?? 'BLANK'),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
